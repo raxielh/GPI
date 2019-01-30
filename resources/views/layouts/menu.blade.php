b840dadb38accb41e5aca62c641418139e238c90
Rodrigo Garcia <rodrigogarciahoyos@gmail.com>
2019-01-30 10:34:52 -0500

    init

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -6,15 +6,7 @@
     abrir_padre();
 @endphp
 
-<li class="{{ Request::is('cargos*') ? 'active' : '' }}">
-    <a href="{{ route('cargos.index') }}"> <i class="material-icons">transfer_within_a_station</i> <span>Cargos</span> </a>
-</li>
-<li class="{{ Request::is('causas*') ? 'active' : '' }}">
-    <a href="{{ route('causas.index') }}"> <i class="material-icons">transfer_within_a_station</i> <span>Causas</span> </a>
-</li>
-<li class="{{ Request::is('empleados_tipos*') ? 'active' : '' }}">
-    <a href="{{ route('empleados_tipos.index') }}"> <i class="material-icons">transfer_within_a_station</i> <span>Empleados_tipos</span> </a>
-</li>
+
 <li class="{{ Request::is('estado_proyecto*') ? 'active' : '' }}">
     <a href="{{ route('estado_proyecto.index') }}"> <i class="material-icons">transfer_within_a_station</i> <span>Estado_proyecto</span> </a>
 </li>
