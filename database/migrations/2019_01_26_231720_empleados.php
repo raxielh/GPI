863246d87498c9be7d1b76c9a02b2d65c4ed6c0c
cahosoft <cahosoft@gmail.com>
2019-02-12 08:44:38 -0500

    t

--- database/migrations/2019_01_26_231720_empleados.php
+++ database/migrations/2019_01_26_231720_empleados.php
@@ -17,6 +17,7 @@ class Empleados extends Migration
             $table->increments('id');
             $table->integer('persona_id')->unsigned();
             $table->integer('cargos_id')->unsigned();
+            $table->integer('direciones_areas_id')->unsigned();
             $table->integer('empleado_estados_id')->unsigned();
             $table->integer('empleados_tipos_id')->unsigned();
             $table->timestamps();
@@ -24,6 +25,7 @@ class Empleados extends Migration
             $table->foreign('cargos_id')->references('id')->on('cargos');
             $table->foreign('empleado_estados_id')->references('id')->on('empleado_estados');
             $table->foreign('empleados_tipos_id')->references('id')->on('empleados_tipos');
+            $table->foreign('direciones_areas_id')->references('id')->on('direciones_areas');
         });
     }
 
