6cfbab0842341b464af57d0e8045eda4e48ed8e5
cahosoft <cahosoft@gmail.com>
2019-02-11 21:46:36 -0500

    Create 2019_02_11_221445_dual.php
    sd

---
+++ database/migrations/2019_02_11_221445_dual.php
@@ -0,0 +1,529 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Dual extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dual', function (Blueprint $table) {
+            $table->string('x');
+
+        });
+
+        DB::table('dual')->insert([
+            'x' => 'x'
+        ]);
+
+            DB::unprepared("
+            CREATE OR REPLACE FUNCTION public.fn_nvl (
+              campo bigint
+            )
+            RETURNS bigint AS
+            $body$
+            DECLARE
+             salida bigint;
+            BEGIN
+
+            salida=0;
+
+                select    CASE WHEN campo is null
+                       THEN 0
+                       ELSE campo
+                       END
+             into STRICT salida
+             from public.dual;
+
+                  return salida;
+
+            END;
+            $body$
+            LANGUAGE 'plpgsql'
+            VOLATILE
+            CALLED ON NULL INPUT
+            SECURITY INVOKER
+            COST 100;
+
+            ALTER FUNCTION public.fn_nvl (campo bigint)
+              OWNER TO postgres;
+
+
+              CREATE OR REPLACE FUNCTION public.fn_nvl (
+              campo time
+            )
+            RETURNS TIME WITHOUT TIME ZONE AS
+            $body$
+            DECLARE
+             salida time;
+            BEGIN
+
+            salida='00:00:00' ;
+
+                select    CASE WHEN campo is null
+                       THEN '00:00:00'
+                       ELSE campo
+                       END
+             into STRICT salida
+             from public.dual;
+
+                  return salida;
+
+            END;
+            $body$
+            LANGUAGE 'plpgsql'
+            VOLATILE
+            CALLED ON NULL INPUT
+            SECURITY INVOKER
+            COST 100;
+
+            ALTER FUNCTION public.fn_nvl (campo time)
+              OWNER TO postgres;
+
+              CREATE OR REPLACE FUNCTION public.fn_nvl (
+              campo date,
+              en_caso_nulo date
+            )
+            RETURNS date AS
+            $body$
+            DECLARE
+             salida DATE;
+            BEGIN
+
+            salida='01/01/1900';
+
+                select    CASE WHEN campo is null
+                       THEN en_caso_nulo
+                       ELSE campo
+                       END
+             into STRICT salida
+             from public.dual;
+
+                  return salida;
+
+            END;
+            $body$
+            LANGUAGE 'plpgsql'
+            VOLATILE
+            CALLED ON NULL INPUT
+            SECURITY INVOKER
+            COST 100;
+
+            ALTER FUNCTION public.fn_nvl (campo date, en_caso_nulo date)
+              OWNER TO postgres;
+
+             CREATE OR REPLACE FUNCTION public.fn_nvl (
+              campo integer,
+              en_caso_nulo integer
+            )
+            RETURNS integer AS
+            $body$
+            DECLARE
+             salida integer;
+            BEGIN
+
+            salida=0;
+
+                select    CASE WHEN campo is null
+                       THEN en_caso_nulo
+                       ELSE campo
+                       END
+             into STRICT salida
+             from public.dual;
+
+                  return salida;
+
+            END;
+            $body$
+            LANGUAGE 'plpgsql'
+            VOLATILE
+            CALLED ON NULL INPUT
+            SECURITY INVOKER
+            COST 100;
+
+            ALTER FUNCTION public.fn_nvl (campo integer, en_caso_nulo integer)
+              OWNER TO postgres;
+
+
+            CREATE OR REPLACE FUNCTION public.fn_nvl (
+              campo numeric
+            )
+            RETURNS numeric AS
+            $body$
+            DECLARE
+             salida numeric;
+            BEGIN
+
+            salida=0;
+
+                select    CASE WHEN campo is null
+                       THEN 0
+                       ELSE campo
+                       END
+             into STRICT salida
+             from public.dual;
+
+                  return salida;
+
+            END;
+            $body$
+            LANGUAGE 'plpgsql'
+            VOLATILE
+            CALLED ON NULL INPUT
+            SECURITY INVOKER
+            COST 100;
+
+            ALTER FUNCTION public.fn_nvl (campo numeric)
+              OWNER TO postgres;
+
+              CREATE OR REPLACE FUNCTION public.fn_nvl (
+              campo varchar
+            )
+            RETURNS varchar AS
+            $body$
+            DECLARE
+             salida varchar;
+            BEGIN
+
+            salida='';
+
+                select    CASE WHEN campo is null
+                       THEN ''
+                       ELSE campo
+                       END
+             into STRICT salida
+             from public.dual;
+
+                  return salida;
+
+            END;
+            $body$
+            LANGUAGE 'plpgsql'
+            VOLATILE
+            CALLED ON NULL INPUT
+            SECURITY INVOKER
+            COST 100;
+
+            ALTER FUNCTION public.fn_nvl (campo varchar)
+              OWNER TO postgres;
+
+             CREATE OR REPLACE FUNCTION public.fn_nvl (
+              campo date
+            )
+            RETURNS date AS
+            $body$
+            DECLARE
+             salida date;
+            BEGIN
+
+            salida='01/01/1900' ;
+
+                select    CASE WHEN campo is null
+                       THEN '01/01/1900'
+                       ELSE campo
+                       END
+             into STRICT salida
+             from public.dual;
+
+                  return salida;
+
+            END;
+            $body$
+            LANGUAGE 'plpgsql'
+            VOLATILE
+            CALLED ON NULL INPUT
+            SECURITY INVOKER
+            COST 100;
+
+            ALTER FUNCTION public.fn_nvl (campo date)
+              OWNER TO postgres;
+
+             CREATE OR REPLACE FUNCTION public.fn_nvl (
+              campo bigint,
+              en_caso_nulo bigint
+            )
+            RETURNS bigint AS
+            $body$
+            DECLARE
+             salida bigint;
+            BEGIN
+
+            salida=0;
+
+                select    CASE WHEN campo is null
+                       THEN en_caso_nulo
+                       ELSE campo
+                       END
+             into STRICT salida
+             from public.dual;
+
+                  return salida;
+
+            END;
+            $body$
+            LANGUAGE 'plpgsql'
+            VOLATILE
+            CALLED ON NULL INPUT
+            SECURITY INVOKER
+            COST 100;
+
+            ALTER FUNCTION public.fn_nvl (campo bigint, en_caso_nulo bigint)
+              OWNER TO postgres;
+
+              CREATE OR REPLACE FUNCTION public.fn_nvl (
+              campo varchar,
+              en_caso_nulo varchar
+            )
+            RETURNS varchar AS
+            $body$
+            DECLARE
+             salida varchar;
+            BEGIN
+
+            salida='';
+
+                select    CASE WHEN campo is null
+                       THEN en_caso_nulo
+                       ELSE campo
+                       END
+             into STRICT salida
+             from public.dual;
+
+                  return salida;
+
+            END;
+            $body$
+            LANGUAGE 'plpgsql'
+            VOLATILE
+            CALLED ON NULL INPUT
+            SECURITY INVOKER
+            COST 100;
+
+            ALTER FUNCTION public.fn_nvl (campo varchar, en_caso_nulo varchar)
+              OWNER TO postgres;
+
+            CREATE OR REPLACE FUNCTION public.fn_nvl (
+              campo numeric,
+              en_caso_nulo numeric
+            )
+            RETURNS numeric AS
+            $body$
+            DECLARE
+             salida numeric;
+            BEGIN
+
+            salida=0;
+
+                select    CASE WHEN campo is null
+                       THEN en_caso_nulo
+                       ELSE campo
+                       END
+             into STRICT salida
+             from public.dual;
+
+                  return salida;
+
+            END;
+            $body$
+            LANGUAGE 'plpgsql'
+            VOLATILE
+            CALLED ON NULL INPUT
+            SECURITY INVOKER
+            COST 100;
+
+            ALTER FUNCTION public.fn_nvl (campo numeric, en_caso_nulo numeric)
+              OWNER TO postgres;
+
+             CREATE OR REPLACE FUNCTION public.fn_nvl (
+              campo integer
+            )
+            RETURNS integer AS
+            $body$
+            DECLARE
+             salida integer;
+            BEGIN
+
+            salida=0;
+
+                select    CASE WHEN campo is null
+                       THEN 0
+                       ELSE campo
+                       END
+             into STRICT salida
+             from public.dual;
+
+                  return salida;
+
+            END;
+            $body$
+            LANGUAGE 'plpgsql'
+            VOLATILE
+            CALLED ON NULL INPUT
+            SECURITY INVOKER
+            COST 100;
+
+            ALTER FUNCTION public.fn_nvl (campo integer)
+              OWNER TO postgres;
+
+             CREATE OR REPLACE FUNCTION public.fn_to_char (
+              camp real
+            )
+            RETURNS varchar AS
+            $body$
+            DECLARE
+             salida varchar;
+            BEGIN
+
+            salida='';
+
+                select    replace ((to_char(camp, '99999999999')), ' ', '')
+             into STRICT salida
+             from public.dual;
+
+                  return salida;
+
+            END;
+            $body$
+            LANGUAGE 'plpgsql'
+            VOLATILE
+            CALLED ON NULL INPUT
+            SECURITY INVOKER
+            COST 100;
+
+            ALTER FUNCTION public.fn_to_char (camp real)
+              OWNER TO postgres;
+
+            CREATE OR REPLACE FUNCTION public.fn_to_char (
+              camp double precision
+            )
+            RETURNS varchar AS
+            $body$
+            DECLARE
+             salida varchar;
+            BEGIN
+
+            salida='';
+
+                select    replace ((to_char(camp, '99999999999')), ' ', '')
+             into STRICT salida
+             from public.dual;
+
+                  return salida;
+
+            END;
+            $body$
+            LANGUAGE 'plpgsql'
+            VOLATILE
+            CALLED ON NULL INPUT
+            SECURITY INVOKER
+            COST 100;
+
+            ALTER FUNCTION public.fn_to_char (camp double precision)
+              OWNER TO postgres;
+
+              CREATE OR REPLACE FUNCTION public.fn_to_char (
+              camp integer
+            )
+            RETURNS varchar AS
+            $body$
+            DECLARE
+             salida varchar;
+            BEGIN
+
+            salida='';
+
+                select    replace ((to_char(camp, '999999999')), ' ', '')
+             into STRICT salida
+             from public.dual;
+
+                  return salida;
+
+            END;
+            $body$
+            LANGUAGE 'plpgsql'
+            VOLATILE
+            CALLED ON NULL INPUT
+            SECURITY INVOKER
+            COST 100;
+
+            ALTER FUNCTION public.fn_to_char (camp integer)
+              OWNER TO postgres;
+
+             CREATE OR REPLACE FUNCTION public.last_day (
+              date
+            )
+            RETURNS date AS
+            $body$
+              SELECT (date_trunc('MONTH', $1) + INTERVAL '1 MONTH - 1 day')::DATE;
+            $body$
+            LANGUAGE 'sql'
+            IMMUTABLE
+            RETURNS NULL ON NULL INPUT
+            SECURITY INVOKER
+            COST 100;
+
+            ALTER FUNCTION public.last_day (date)
+              OWNER TO postgres;
+
+             CREATE OR REPLACE FUNCTION public.restar_fechas (
+              fecha_ini date,
+              fecha_fin date
+            )
+            RETURNS varchar AS
+            $body$
+            DECLARE
+             salida varchar;
+            BEGIN
+
+            salida='';
+
+
+            SELECT CASE WHEN (date_part('year', age(fecha_ini, fecha_fin)) > 0) THEN
+                              date_part('year', age(fecha_ini, fecha_fin)) || CASE WHEN (date_part('year', age(fecha_ini, fecha_fin)) = 1) THEN ' año  '
+                                                                                                             WHEN (date_part('year', age(fecha_ini, fecha_fin)) > 1) THEN ' años ' END
+                        ELSE '' END ||
+                   CASE WHEN (date_part('month', age(fecha_ini, fecha_fin)) > 0) THEN
+                              date_part('month', age(fecha_ini, fecha_fin)) || CASE WHEN (date_part('month', age(fecha_ini, fecha_fin)) = 1) THEN ' mes  '
+                                                                                                              WHEN (date_part('month', age(fecha_ini, fecha_fin)) > 1) THEN ' meses ' END
+                        ELSE '' END ||
+                   CASE WHEN (date_part('day', age(fecha_ini, fecha_fin)) > 0) THEN
+                              date_part('day', age(fecha_ini, fecha_fin)) || CASE WHEN (date_part('day', age(fecha_ini, fecha_fin)) = 1) THEN ' dia  '
+                                                                                                            WHEN (date_part('day', age(fecha_ini, fecha_fin)) > 1) THEN ' dias ' END
+                        ELSE '' END AS Periodos
+                         into STRICT salida
+                        from public.dual;
+
+
+                  return salida;
+
+            END;
+            $body$
+            LANGUAGE 'plpgsql'
+            VOLATILE
+            CALLED ON NULL INPUT
+            SECURITY INVOKER
+            COST 100;
+
+            ALTER FUNCTION public.restar_fechas (fecha_ini date, fecha_fin date)
+              OWNER TO postgres;
+
+
+            ");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dual');
+    }
+}
